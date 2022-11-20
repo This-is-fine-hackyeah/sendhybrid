6369b873a9d7a2f78ca1b75de60d96731ae16961
Bartłomiej Kurzeja <bartlomiej@kurzeja.it>
2022-11-20 05:05:19 +0100

    Report is now nullable

--- backend/app/app/models/report.py
+++ backend/app/app/models/report.py
@@ -6,61 +6,61 @@ from app.db.base_class import Base
 
 class Report(Base):
     """What checks failed"""
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     document_id = Column(Integer, ForeignKey("document.id"), nullable=False)
-    document = relationship("Document")
+    document = relationship("Document", back_populates="report")
 
-    format = Column(Boolean, nullable=False)
-    orientation = Column(Boolean, nullable=False)
-    pdf_version = Column(Boolean, nullable=False)
-    min_margin_top = Column(Boolean, nullable=False)
-    min_margin_bottom = Column(Boolean, nullable=False)
-    min_margin_side = Column(Boolean, nullable=False)
+    format = Column(Boolean, nullable=True)
+    orientation = Column(Boolean, nullable=True)
+    pdf_version = Column(Boolean, nullable=True)
+    min_margin_top = Column(Boolean, nullable=True)
+    min_margin_bottom = Column(Boolean, nullable=True)
+    min_margin_side = Column(Boolean, nullable=True)
 
-    protections_allowed = Column(Boolean, nullable=False)
+    protections_allowed = Column(Boolean, nullable=True)
 
     # filename
-    name_banned_symbols = Column(Boolean, nullable=False)
-    allow_leading_space = Column(Boolean, nullable=False)
-    allow_trailing_space = Column(Boolean, nullable=False)
-    encoding = Column(Boolean, nullable=False)
-    filename_max_length = Column(Boolean, nullable=False)
+    name_banned_symbols = Column(Boolean, nullable=True)
+    allow_leading_space = Column(Boolean, nullable=True)
+    allow_trailing_space = Column(Boolean, nullable=True)
+    encoding = Column(Boolean, nullable=True)
+    filename_max_length = Column(Boolean, nullable=True)
 
     # optim
-    composite_pdf = Column(Boolean, nullable=False)
-    allow_unused_links_etc = Column(Boolean, nullable=False)
-    allow_hidden_layer_info = Column(Boolean, nullable=False)
+    composite_pdf = Column(Boolean, nullable=True)
+    allow_unused_links_etc = Column(Boolean, nullable=True)
+    allow_hidden_layer_info = Column(Boolean, nullable=True)
 
     # fonts
-    embedded_font = Column(Boolean, nullable=False)
-    min_font_single_color_single_elem = Column(Boolean, nullable=False)
-    min_font_single_color_double_elem = Column(Boolean, nullable=False)
-    min_font_multi_color_single_elem = Column(Boolean, nullable=False)
-    min_font_multi_color_double_elem = Column(Boolean, nullable=False)
+    embedded_font = Column(Boolean, nullable=True)
+    min_font_single_color_single_elem = Column(Boolean, nullable=True)
+    min_font_single_color_double_elem = Column(Boolean, nullable=True)
+    min_font_multi_color_single_elem = Column(Boolean, nullable=True)
+    min_font_multi_color_double_elem = Column(Boolean, nullable=True)
 
     # lines
-    min_line_thickness = Column(Boolean, nullable=False)
-    min_line_thickness_other = Column(Boolean, nullable=False)
+    min_line_thickness = Column(Boolean, nullable=True)
+    min_line_thickness_other = Column(Boolean, nullable=True)
 
-    active_forms = Column(Boolean, nullable=False)
+    active_forms = Column(Boolean, nullable=True)
 
-    color_mode = Column(Boolean, nullable=False)
-    text_color = Column(Boolean, nullable=False)
+    color_mode = Column(Boolean, nullable=True)
+    text_color = Column(Boolean, nullable=True)
 
     # graphics
-    min_dpi = Column(Boolean, nullable=False)
-    graphic_scale = Column(Boolean, nullable=False)
-    graphic_color_mode = Column(Boolean, nullable=False)
+    min_dpi = Column(Boolean, nullable=True)
+    graphic_scale = Column(Boolean, nullable=True)
+    graphic_color_mode = Column(Boolean, nullable=True)
 
     # address
-    name_max_length = Column(Boolean, nullable=False)
-    name_cont_max_length = Column(Boolean, nullable=False)
-    street_max_length = Column(Boolean, nullable=False)
-    building_number_max_length = Column(Boolean, nullable=False)
-    local_number_max_length = Column(Boolean, nullable=False)
-    postal_code_max_length = Column(Boolean, nullable=False)
-    city_max_length = Column(Boolean, nullable=False)
-    country_max_length = Column(Boolean, nullable=False)
+    name_max_length = Column(Boolean, nullable=True)
+    name_cont_max_length = Column(Boolean, nullable=True)
+    street_max_length = Column(Boolean, nullable=True)
+    building_number_max_length = Column(Boolean, nullable=True)
+    local_number_max_length = Column(Boolean, nullable=True)
+    postal_code_max_length = Column(Boolean, nullable=True)
+    city_max_length = Column(Boolean, nullable=True)
+    country_max_length = Column(Boolean, nullable=True)
 
-    sender_info_max_length = Column(Boolean, nullable=False)
-    signature_act_max_length = Column(Boolean, nullable=False)
+    sender_info_max_length = Column(Boolean, nullable=True)
+    signature_act_max_length = Column(Boolean, nullable=True)
